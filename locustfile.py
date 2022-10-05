373a8b0971af9326213989dc4aa1e09b22236688
Jonathan Green <jonathan@razzard.com>
2022-10-05 16:38:09 -0300

    Initial load tests

---
+++ locustfile.py
@@ -0,0 +1,39 @@
+from urllib.parse import urlparse
+
+from gevent.pool import Pool
+
+from locust import FastHttpUser, task
+
+
+class PalaceUser(FastHttpUser):
+
+    host = "https://registry.palaceproject.io/"
+
+    def fetch(self, url):
+        parsed = urlparse(url)
+        name = f"{parsed.hostname}/[library]/authentication_document"
+        self.client.get(url, name=name)
+
+    @task
+    def first_open_app(self):
+        """
+        Simulates a user opening the app for the first time.
+        """
+        response = self.client.get("/libraries")
+        json = response.json()
+        if "catalogs" not in json:
+            response.failure("No catalogs key")
+
+        authentication_documents = []
+        for catalog in json["catalogs"]:
+            for link in catalog["links"]:
+                if (
+                    "type" in link
+                    and link["type"] == "application/vnd.opds.authentication.v1.0+json"
+                ):
+                    authentication_documents.append(link["href"])
+
+        pool = Pool()
+        for href in authentication_documents:
+            pool.spawn(self.fetch, href)
+        pool.join()
