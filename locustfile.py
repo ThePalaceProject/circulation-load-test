382b219c165d7a85d8b363c42f3ad6d63a0af0b4
Mark Raynsford <code@io7m.com>
2023-04-03 16:11:00 +0000

    Fix the locustfile and prevent future lint-related breakage.

---
+++ locustfile.py
@@ -0,0 +1,3 @@
+# Note: All imports in this file are used by Locust to locate tests.
+from circulation_load_test.cm.basic import CMTests  # noqa: autoflake
+from circulation_load_test.registry.registry import RegistryTests  # noqa: autoflake
